b3eecfbb8b61abfe7201e2abbd9dc303e5c090d9
Taylor Fausak <taylor@fausak.me>
2019-10-05 14:25:19 -0400

    Add pop function

--- Main.hs
+++ Main.hs
@@ -2,13 +2,18 @@ type DiceChoice = [ Bool ]
 type DiceVals   = [ Integer ]
 type DiceState = (DiceVals, Integer)
 
+pop :: DiceChoice
+    -> DiceVals
+    -> Maybe ((Bool, Integer), (DiceChoice, DiceVals))
+pop [] [] = Nothing
+pop (chosen:choices) (v:vs) = Just ((chosen, v), (choices, vs))
+pop (_:_) [] = error "Invariant violated: missing val"
+pop [] (_:_) = error "Invariant violated: missing choice"
+
 allRolls :: DiceChoice -> DiceState -> [ DiceState ]
-allRolls [] ([], n) = [ ([], n-1) ]
-allRolls [] (_:_, _) =
-  error "Invariant violated: choices must be same length as vals"
-allRolls (_:_) ([], _) =
-  error "Invariant violated: choices must be same length as vals"
-allRolls (chosen:choices) (v:vs, n) =
+allRolls choices (vs, n) = case pop choices vs of
+  Nothing -> [ ([], n-1) ]
+  Just ((chosen, v), (choices, vs)) ->
     allRolls choices (vs, n-1) >>=
         \(roll,_) -> [ (d:roll,  n-1) | d <- rollList ]
           where rollList = if chosen then [v] else [ 1..6 ]
