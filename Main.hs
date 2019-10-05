0e5312ffefcda2a83138938a12cdb45e2869faf5
Taylor Fausak <taylor@fausak.me>
2019-10-05 14:26:56 -0400

    Rearrange arguments

--- Main.hs
+++ Main.hs
@@ -10,16 +10,17 @@ pop (_:_) [] = error "Invariant violated: missing val"
 pop [] (_:_) = error "Invariant violated: missing choice"
 
 allRolls :: DiceChoice
-         -> (DiceVals, Integer)
+         -> DiceVals
+         -> Integer
          -> [ (DiceVals, Integer) ]
-allRolls choices (vs, n) = case pop choices vs of
+allRolls choices vs n = case pop choices vs of
   Nothing -> [ ([], n-1) ]
   Just ((chosen, v), (choices, vs)) ->
-    allRolls choices (vs, error "Didn't expect to use") >>=
+    allRolls choices vs (error "Didn't expect to use") >>=
         \(roll,_) -> [ (d:roll,  n-1) | d <- rollList ]
           where rollList = if chosen then [v] else [ 1..6 ]
 
 main =
   let diceChoices = [ False, True, True, False, False ]
       diceVals = [ 6, 4, 4, 3, 1 ]
-  in mapM_ print $ allRolls diceChoices (diceVals, 2)
+  in mapM_ print $ allRolls diceChoices diceVals 2
