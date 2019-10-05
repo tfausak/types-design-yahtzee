28b5717f27bc48911e80a82e6743352921956a94
Taylor Fausak <taylor@fausak.me>
2019-10-05 14:37:28 -0400

    Keep the better version

--- Main.hs
+++ Main.hs
@@ -1,19 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
 
 type DiceVals   = [ Integer ]
-type DiceTurn  = [(Bool, Integer)]
-
-allRolls :: DiceTurn
-         -> Integer
-         -> [ (DiceVals, Integer) ]
-allRolls t n = [ (vals, n-1) | vals <- allRollsNoN t ]
-
-allRollsNoN = allRollsBetter . map fromTurn
-
 data DiceChoice = Keep Integer | Reroll
-
-fromTurn :: (Bool, Integer) -> DiceChoice
-fromTurn (chosen, v) = if chosen then Keep v else Reroll
 
 allRollsBetter :: [DiceChoice] -> [ DiceVals ]
 allRollsBetter = mapM $ \case
@@ -21,6 +9,5 @@ allRollsBetter = mapM $ \case
   Keep v -> [v]
 
 main =
-  let diceChoices = [ False, True, True, False, False ]
-      diceVals = [ 6, 4, 4, 3, 1 ]
-  in mapM_ print $ allRolls (zip diceChoices diceVals) 2
+  let diceVals = [ Reroll, Keep 4, Keep 4, Reroll, Reroll ]
+  in mapM_ print $ allRollsBetter diceVals
