d973cb94a4a896e3fef36901b90155efcc8fa43d
Taylor Fausak <taylor@fausak.me>
2019-10-05 14:23:32 -0400

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,16 @@
+type DiceChoice = [ Bool ]
+type DiceVals   = [ Integer ]
+type DiceState = (DiceVals, Integer)
+
+allRolls :: DiceChoice -> DiceState -> [ DiceState ]
+allRolls [] ([], n) = [ ([], n-1) ]
+allRolls [] _ = undefined
+allRolls (chosen:choices) (v:vs, n) =
+    allRolls choices (vs, n-1) >>=
+        \(roll,_) -> [ (d:roll,  n-1) | d <- rollList ]
+          where rollList = if chosen then [v] else [ 1..6 ]
+
+main =
+  let diceChoices = [ False, True, True, False, False ]
+      diceVals = [ 6, 4, 4, 3, 1 ]
+  in mapM_ print $ allRolls diceChoices (diceVals, 2)
---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Types, Design, Yahtzee
+
+<http://h2.jaguarpaw.co.uk/posts/good-design-and-type-safety-in-yahtzee/>
